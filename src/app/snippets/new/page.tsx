c94ce48f4e3e93b16f5922377e40fd2f57c5b18e
arroyostack <arroyostack@gmail.com>
2024-01-19 17:28:43 +0100

    Feat: Prisma model for Snippet.

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,11 @@
+export default function SnippetCreatePage() {
+    return (
+        <>
+            <h1>Creat page</h1>
+            <form className="flex flex-row gap-5">
+                <input type="text" />
+                <textarea />
+            </form>
+        </>
+    );
+}
