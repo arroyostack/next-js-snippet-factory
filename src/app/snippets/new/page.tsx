8592636f449021a6108e517447cb9e5a10a2e4fb
arroyostack <arroyostack@gmail.com>
2024-01-19 19:51:52 +0100

    Feat: Create form and server action implemented.

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,10 +1,60 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+
 export default function SnippetCreatePage() {
+
+    async function createSnippet( formData: FormData ) {
+        // Make clear this is a servet action
+        'use server';
+        // User enter valid input
+
+        const title = formData.get( 'title' ) as string;
+        const code = formData.get( 'code' ) as string;
+
+        // Take input a create a new record in the database.
+        const snippet = await db.snippet.create( {
+            data: {
+                title,
+                code
+            }
+        } );
+
+        console.log( snippet );
+        // Redirect user back to the root route
+        redirect( '/' );
+
+    }
+
     return (
         <>
-            <h1>Creat page</h1>
-            <form className="flex flex-row gap-5">
-                <input type="text" />
-                <textarea />
+            <form
+                action={ createSnippet }
+                className="flex flex-col gap-5">
+                <h3 className="font-bold m-3">Create Snippet</h3>
+                <div className="flex flex-col gap-4">
+                    <label className="w-12" htmlFor="title">
+                        Title
+                    </label>
+                    <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        className="border rounded p-2 w-full"
+                    />
+                </div>
+                <div className="flex flex-col gap-4">
+                    <label className="w-12" htmlFor="code">
+                        Code
+                    </label>
+                    <textarea
+                        id="code"
+                        name="code"
+                        className="border rounded p-2 w-full"
+                    />
+                </div>
+
+                <button type="submit" className="rounded p-2 bg-blue-200">Submit</button>
             </form>
         </>
     );
